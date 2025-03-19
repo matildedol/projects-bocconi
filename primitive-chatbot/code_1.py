108dd2fe48ac2fc0aa8b6da68593ea0c76aa32ba
matildedol <matilde.dolfato@studbocconi.it>
2025-03-19 22:32:49 +0100

    retrived responses!

--- primitive-chatbot/code_1.py
+++ primitive-chatbot/code_1.py
@@ -7,7 +7,13 @@ import sys
 import time
 import datetime
 from scipy.sparse import vstack
-
+import pickle
+import os
+from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import SmoothingFunction
+'''
+should i use a class?? bo! 
+'''
 
 nlp = spacy.load("en_core_web_sm")   #https://spacy.io/usage/models, multi-language, chosen for accuracy
 
@@ -17,9 +23,26 @@ print('starting time:', now)
 train_df=pd.read_csv('train_responses.csv')
 test_df=pd.read_csv('dev_responses.csv')
 
+
 # we build a vocabulary with dependency-based word bigrams 'lemma_head' from a list of documents 'corpus'
-def build_vocabulary(corpus):                                                                                                    
-    vocabulary = set(['{}_{}'.format(token.lemma_.lower(), token.head.lemma_.lower()) for doc in corpus for token in nlp(doc)])             # in our case, docs are prompts
+def build_vocabulary(corpus, cache_file='vocabulary_cache.pkl'):
+    # Check if cache file exists
+    if os.path.exists(cache_file):
+        print(f"loading vocabulary from cache: {cache_file}")
+        with open(cache_file, 'rb') as f:
+            vocabulary = pickle.load(f)
+        return vocabulary
+    
+    # Build vocabulary if no cache exists
+    print("building vocabulary from scratch...")
+    vocabulary = set(['{}_{}'.format(token.lemma_.lower(), token.head.lemma_.lower()) 
+                    for doc in corpus for token in nlp(doc)])
+    
+    # Save vocabulary to cache
+    print(f"saving vocabulary to cache: {cache_file}")
+    with open(cache_file, 'wb') as f:
+        pickle.dump(vocabulary, f)
+    
     return vocabulary
 
 def get_discrete_rep(corpus, vocab, visualize):
@@ -42,48 +65,82 @@ def get_discrete_rep(corpus, vocab, visualize):
 
     return reps_list
 
-def get_bestmatch(queries, keys, metric=cosine_similarity):    
+def get_bestmatch(queries, keys, keys_df, metric=cosine_similarity):    
     queries_matrix = vstack(queries)
     keys_matrix = vstack(keys)
     print('building similarity matrix...')
     similarity_matrix = metric(queries_matrix, keys_matrix)
     print('retrieving best matches...')
     best_match_indices = similarity_matrix.argmax(axis=1)
+    #instaed of keys[i] i want to put the value in the keys_df under responses at index i
     
-    return [keys[i] for i in best_match_indices]                                  # list of best matches orderd as queries list
+    return [keys_df.iloc[i]['model_response'] for i in best_match_indices]                                  # list of best matches orderd as queries list
+
+# do a function that takes the rep, goes to keys_df (aggiornato),
+# takes the corresponding response, stores the response in a pivot_dict as the value to the QUERIES! rep
+# then create a list with all responses in order and add to df
+# (i think there's a cleaner way of doing this but whatever)
+
+# def retrieve_responses(best_matches_reps, keys_df): 
+#     print('retrieving corresponding reponses...')
+#     computed_responses=[]
+#     keys_reps_list=keys_df['vector_reps'].to_list()
+#     for q in best_matches_reps:
+#         for k in keys_reps_list:
+#             if k.all()==q.all():
+#                 # let's try to do it again relying on order implicitly remaining the same  
+#                 computed_responses.append(keys_df.iloc[k]['model_response'])
+#     if len(computed_responses)!=len(best_matches_reps):
+#         sys.exit('watch out! the list of computed responses has a wrong length :/')
+#     return computed_responses
 
 # def compute_cosinesim(query,key):
 #     cos_sim = np.dot(query,key) / (np.linalg.norm(query)*np.linalg.norm(key))
 #     return cos_sim
 
-def get_results(queries_df, keys_df):
+def get_results(queries_df, keys_df, use_cache=True):
     print("let's start!\n")
     keys_prompts = keys_df['user_prompt'].to_list()
     queries_prompts = queries_df['user_prompt'].to_list()
+    
     print('building vocabulary...\n')
-    my_vocabulary=build_vocabulary(keys_prompts)
-    voc_time=time.time() - start_time
+    cache_file = 'vocabulary_cache.pkl'
+    if not use_cache and os.path.exists(cache_file):
+        os.remove(cache_file)  # Delete cache if not using it
+    
+    vocab_start_time = time.time()
+    my_vocabulary = build_vocabulary(keys_prompts, cache_file)
+    vocab_end_time = time.time()
+    voc_time = vocab_end_time - vocab_start_time
     print(f'vocabulary built! time: {voc_time:.2f} s\n')
+    print('computing representations...\n')
     train_reps=get_discrete_rep(keys_prompts, my_vocabulary, 'n')
+    keys_df['vector_reps']=train_reps #add representations to df to pivot later
     train_time=time.time() - voc_time
     print(f'\ntraining set representations: done! time: {train_time:.2f} s\n')
     test_reps=get_discrete_rep(queries_prompts, my_vocabulary, 'n')
     test_time=time.time() - train_time
     print(f'test set representations: done! time: {test_time:.2f} s\n')
-    matches=get_bestmatch(test_reps, train_reps, cosine_similarity)
+    matches=get_bestmatch(test_reps, train_reps, keys_df, cosine_similarity)
     match_time=time.time() - test_time
     print(f'matches retrieved! time: {match_time:.2f} s\n')
-    queries_df['best_matches']=matches
+    queries_df['retrieved_response']=matches
+    #responses=retrieve_responses(matches, keys_df)
+    #queries_df['computed_responses']=responses
     end_time=time.time() - start_time
     return queries_df, end_time
 
 
-results, run_time = get_results(test_df, train_df)
-
-print(f'Running time: {run_time:.2f}\nResults:', results.head())
-
 # compute BLEU score to assess performance
-def compute_BLEU():
-    pass
+# def compute_BLEU(data):
+#     smoothingfunction = SmoothingFunction() # if you want to know more about smoothing functions: https://aclanthology.org/W14-3346.pdf
+#     if data[data['model_response'].isna() | data['retrieved_response'].isna()]:
+#         print('NaNs values found!')
+#     data['bleu_score'] = data.apply(lambda x: sentence_bleu([x['model_response'].split()], x['retrieved_response'].split(), weights=(0.5, 0.5, 0, 0), smoothing_function=smoothingfunction.method3), axis=1)
+#     end_time=time.time() - start_time
+#     return data, end_time
 
 
+results,run_time=get_results(test_df, train_df)
+#evaluation, run_time = compute_BLEU(results)
+print(f'Running time: {run_time:.2f}\nResults:', results.head())
